46fa0c2161e4ca302e61216225bdd634a0803a46
ipadyptab-byte <ipadyptab@gmail.com>
2025-09-07 16:41:54 +0530

    Update schema.ts

--- shared/schema.ts
+++ shared/schema.ts
@@ -1,5 +1,5 @@
 import { sql } from "drizzle-orm";
-import { pgTable, text, integer, real, serial, boolean, timestamp } from "drizzle-orm/pg-core";
+import { pgTable, text, integer, real, serial, boolean, timestamp, bytea } from "drizzle-orm/pg-core";
 import { createInsertSchema } from "drizzle-zod";
 import { z } from "zod";
 
